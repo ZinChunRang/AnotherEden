cc22f1bb231d0a9f8e6f2d921857a16b21b1f0de
ZinChunRang <s7e004@naver.com>
2021-12-22 11:04:09 +0900

    test

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { MainComponent } from './main/main.component';
 
 const routes: Routes = [
-  { path: 'AnotherEden', component: MainComponent },  // main page
+  { path: '', component: MainComponent },  // main page
 ];
 
 @NgModule({
